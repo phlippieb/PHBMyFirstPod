81ae8e7052ea522263fa8712a07867a51ea70353
phlippieb <phlippie.bosman@gmail.com>
2019-08-08 14:53:52 +0200

    Reference pod in example

--- Example/PHBMyFirstPod/ViewController.swift
+++ Example/PHBMyFirstPod/ViewController.swift
@@ -7,18 +7,12 @@
 //
 
 import UIKit
+import PHBMyFirstPod
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        MyFirstPod.foo()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
