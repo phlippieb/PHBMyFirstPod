ee08d2d5183a9ec3f6ddd98af1b77a16a7cb6aa2
phlippieb <phlippie.bosman@gmail.com>
2019-08-08 14:51:41 +0200

    Fix warnings etc

---
+++ Source/MyFirstPod.swift
@@ -0,0 +1,14 @@
+//
+//  MyFirstPod.swift
+//  PHBMyFirstPod
+//
+//  Created by Phlippie Bosman on 2019/08/08.
+//
+
+import Foundation
+
+final class MyFirstPod {
+    static func foo() {
+        NSLog("My first pod")
+    }
+}
